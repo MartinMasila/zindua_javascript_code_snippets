aa6cc4118708bb2ef2a78ef842da98b4ef66a4b8
MartinMasila <masilamartin@yahoo.com>
2024-03-05 20:24:07 +0300

    DataTypes

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+let x = 7;
+console.log(typeof x);
+
+let y = true;  // Can be True or False
+console.log(typeof y);
+
+let z = 'hello world'; // anything inside of single quotes
+console.log(typeof z);
+
+let a;
+console.log(a);
+console.log(typeof a);
