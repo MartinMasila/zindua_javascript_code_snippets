153a16eb685cba45f960085b560662b26e6b41c2
MartinMasila <masilamartin@yahoo.com>
2024-03-05 20:13:03 +0300

    Variable Rules

---
+++ variables.js
@@ -0,0 +1,12 @@
+let x = 7;
+let y = 3;
+let z = x + y;
+console.log('Answer is ' + z);
+
+// Variable names must be descriptive and should represent the thing you want to store
+
+// Use camel casing, the first name of the variable should be lowercase and the subsequent words appended should have a capital letter
+
+let firstNumber = 7;
+let secondNumber = 3;
+let firstName = 'Martin';
